e37757ee7d6e16c577ad1eee382832c6e613b868
deeraj <theepshi123@gmail.com>
2020-11-03 14:43:02 +0530

    [Rename]dlock to pu2

--- Makefile
+++ Makefile
@@ -4,10 +4,10 @@ DEPS = auth.h handlers.h
 LDEPS = `pkg-config --libs --cflags gtk+-2.0`
 LSTATIC = -lpam -lpam_misc
 OBJ = auth.o handlers.o
-MAIN = dlock.c
+MAIN = pu2.c
 
 %.o: %.c $(DEPS)
 	$(CC) $(CFLAGS) $(LDEPS) $(LSTATIC) -c -o $@ $<
 
-dlock: $(OBJ)
+pu2: $(OBJ)
 	gcc $(CFLAGS) $(LDEPS) $(LSTATIC) -o $@ $(MAIN) $^
--- pu2.c
+++ pu2.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
 	GtkWidget *window;
 	gtk_init(&argc, &argv);
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(window), "D-LOCK");
+	gtk_window_set_title(GTK_WINDOW(window), "pu2");
 	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
 	gtk_window_fullscreen(GTK_WINDOW(window));
 	gtk_window_set_deletable(GTK_WINDOW(window), FALSE);
