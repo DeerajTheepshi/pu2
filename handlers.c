20d38e8262d2fb28f12b13130a261b2eaab550e3
deeraj <theepshi123@gmail.com>
2020-11-03 15:09:08 +0530

    [Fix]Support backspace on input

--- handlers.c
+++ handlers.c
@@ -1,6 +1,15 @@
 #include "handlers.h"
-
+#include <stdio.h>
 char password[25];
+
+void handleInput(guint keyVal){
+	char key = (char)keyVal;
+	strncat(password, &key, 1);
+}
+
+void handleBackspace(){
+	password[strlen(password)-1] = '\0';
+}
 
 gboolean loginHandler(GtkWidget *window, GdkEventKey *event, gpointer data){
 	guint keyVal = event->keyval;
@@ -8,9 +17,10 @@ gboolean loginHandler(GtkWidget *window, GdkEventKey *event, gpointer data){
 	keyVal==GDK_KEY_Control_L || keyVal==GDK_KEY_Control_R || 
 	keyVal==GDK_KEY_Caps_Lock){
 		return FALSE;
+	} else if(keyVal==GDK_KEY_BackSpace){
+		handleBackspace();
 	} else if(keyVal!=GDK_KEY_Return){
-		char key = (char)keyVal;
-		strncat(password, &key, 1);
+		handleInput(keyVal);
 	} else if(keyVal==GDK_KEY_Return) {
 		char *user = "diprivi";
 		if(login(user, password))
